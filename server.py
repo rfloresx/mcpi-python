475338065b68c47bce13238c716102bca4398d4a
Casey Richardson <caseybrichardson@icloud.com>
2013-10-08 00:47:48 -0500

    Updated Interface
    Added a few features to the interface and created the delete.py script.
    Also added the .pyc files to the ignore.

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ Created on Oct 4, 2013
 
 #address = "129.113.132.58"
 #address = "192.168.1.3"
-address = "129.113.130.131"
+address = "192.168.2.2"
 
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create( address)
