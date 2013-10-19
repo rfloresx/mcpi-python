e6ad18b22484e859b5ef2c9d061716fb9e11a6b9
otrebor4 <rflores4580@gmail.com>
2013-10-18 19:24:51 -0500

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,6 @@
 
-
-#address = "127.0.0.1"
+address = "127.0.0.1"
 #address = "129.113.130.131"
-address = "129.113.130.228"
+#address = "129.113.130.228"
 import mcpi.minecraft as minecraft
 mc = minecraft.Minecraft.create(address)
